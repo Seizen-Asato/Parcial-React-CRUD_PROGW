7777f4762923f2058c61a52426951b85fec9cd7e
t1629 <walterburique@gmail.com>
2026-06-13 10:51:46 -0300

    Agregado de componente DtailPost con estilos Boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "./components/layout/App.css";
 import "./components/layout/index.css";
-import Prueba from "./pages/Prueba";
+import DetailPost from "./pages/DetailPost";
 function App() {
   return (
     <>
       <div className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/prueba" element={<Prueba />} />
+          <Route path="/detailPost/:id" element={<DetailPost />} />
         </Routes>
       </div>
     </>
