793a70cb435374fe846e932ddba08042f7158208
Mario Seizen Asato <marioseizenasato@gmail.com>
2026-06-13 22:41:58 -0300

    fix(merge): Resolvi conflicto que tenia el archivo App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "./components/layout/App.css";
 import "./components/layout/index.css";
-// import EditPost from "./pages/EditPost";
+import EditPost from "./pages/EditPost";
+
 function App() {
   return (
     <>
       <div className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/EditPost" element={<EditPost />} /> */}
+          <Route path="/ EditPost" element={<EditPost />} />
         </Routes>
       </div>
     </>
